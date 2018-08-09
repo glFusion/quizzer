902367c1db9e4a1d8729ea46350ca1213bd8c487
Lee Garner <lee@leegarner.com>
2018-08-09 14:56:04 -0700

    Fix references to Forms plugin

--- public_html/index.php
+++ public_html/index.php
@@ -4,20 +4,19 @@
 *   Used to either display a specific form, or to save the user-entered data.
 *
 *   @author     Lee Garner <lee@leegarner.com>
-*   @copyright  Copyright (c) 2010-2018 Lee Garner <lee@leegarner.com>
-*   @package    forms
-*   @version    0.3.1
+*   @copyright  Copyright (c) 2018 Lee Garner <lee@leegarner.com>
+*   @package    quizzer
+*   @version    0.0.1
 *   @license    http://opensource.org/licenses/gpl-2.0.php
 *               GNU Public License v2 or later
 *   @filesource
 */
 
 require_once '../lib-common.php';
-if (!in_array('forms', $_PLUGINS)) {
+if (!in_array('quizzer', $_PLUGINS)) {
     COM_404();
 }
 
-USES_forms_functions();
 
 $content = '';
 $action = '';
