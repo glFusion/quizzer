4c2a847150765215e12e51db5f428996dd61ee21
Lee Garner <lee@leegarner.com>
2019-04-05 15:01:29 -0700

    Add missing answers table to installation and removal

--- autoinstall.php
+++ autoinstall.php
@@ -3,7 +3,7 @@
 *   Provides automatic installation of the Quizzer plugin.
 *
 *   @author     Lee Garner <lee@leegarner.com>
-*   @copyright  Copyright (c) 2010 Lee Garner <lee@leegarner.com>
+*   @copyright  Copyright (c) 2018 Lee Garner <lee@leegarner.com>
 *   @package    quizzer
 *   @version    0.0.1
 *   @license    http://opensource.org/licenses/gpl-2.0.php
@@ -23,9 +23,9 @@ require_once __DIR__ . '/functions.inc';
 /** Include database definitions */
 require_once __DIR__ . '/sql/' . $_DB_dbms . '_install.php';
 
-/** Plugin installation options
-*   @global array $INSTALL_plugin['quizzer']
-*/
+/** Plugin installation options.
+ *  @global array $INSTALL_plugin['quizzer']
+ */
 $INSTALL_plugin['quizzer'] = array(
     'installer' => array('type' => 'installer',
             'version' => '1',
@@ -38,45 +38,59 @@ $INSTALL_plugin['quizzer'] = array(
             'url'       => $_CONF_QUIZ['pi_url'],
             'display'   => $_CONF_QUIZ['pi_display_name']),
 
-    array('type' => 'table',
-            'table'     => $_TABLES['quizzer_quizzes'],
-            'sql'       => $_SQL['quizzer_quizzes']),
-
-    array('type' => 'table',
-            'table'     => $_TABLES['quizzer_questions'],
-            'sql'       => $_SQL['quizzer_questions']),
-
-    array('type' => 'table',
-            'table'     => $_TABLES['quizzer_results'],
-            'sql'       => $_SQL['quizzer_results']),
-
-    array('type' => 'table',
-            'table'     => $_TABLES['quizzer_values'],
-            'sql'       => $_SQL['quizzer_values']),
-
-   array('type' => 'group',
-            'group' => 'quizzer Admin',
-            'desc' => 'Users in this group can administer the Quizzer plugin',
-            'variable' => 'admin_group_id',
-            'admin' => true,
-            'addroot' => true),
-
-    array('type' => 'feature',
-            'feature' => 'quizzer.admin',
-            'desc' => 'Quizzer Administration access',
-            'variable' => 'admin_feature_id'),
-
-    array('type' => 'mapping',
-            'group' => 'admin_group_id',
-            'feature' => 'admin_feature_id',
-            'log' => 'Adding Admin feature to the admin group'),
+    array(
+        'type'  => 'table',
+        'table' => $_TABLES['quizzer_quizzes'],
+        'sql'   => $_SQL['quizzer_quizzes'],
+    ),
+    array(
+        'type'  => 'table',
+        'table' => $_TABLES['quizzer_questions'],
+        'sql'   => $_SQL['quizzer_questions'],
+    ),
+    array(
+        'type'  => 'table',
+        'table' => $_TABLES['quizzer_answers'],
+        'sql'   => $_SQL['quizzer_answers'],
+    ),
+    array(
+        'type'  => 'table',
+        'table' => $_TABLES['quizzer_results'],
+        'sql'   => $_SQL['quizzer_results'],
+    ),
+    array(
+        'type'  => 'table',
+        'table' => $_TABLES['quizzer_values'],
+        'sql'   => $_SQL['quizzer_values'],
+    ),
+    array(
+        'type'      => 'group',
+        'group'     => 'quizzer Admin',
+        'desc'      => 'Users in this group can administer the Quizzer plugin',
+        'variable'  => 'admin_group_id',
+        'admin'     => true,
+        'addroot'   => true,
+    ),
+    array(
+        'type'      => 'feature',
+        'feature'   => 'quizzer.admin',
+        'desc'      => 'Quizzer Administration access',
+        'variable'  => 'admin_feature_id',
+    ),
+    array(
+        'type'      => 'mapping',
+        'group'     => 'admin_group_id',
+        'feature'   => 'admin_feature_id',
+        'log'       => 'Adding Admin feature to the admin group',
+    ),
 );
 
 /**
-* Puts the datastructures for this plugin into the glFusion database
-* Note: Corresponding uninstall routine is in functions.inc
-* @return   boolean True if successful False otherwise
-*/
+ * Puts the datastructures for this plugin into the glFusion database.
+ * Note: Corresponding uninstall routine is in functions.inc.
+ *
+ * @return  boolean True if successful False otherwise
+ */
 function plugin_install_quizzer()
 {
     global $INSTALL_plugin, $_CONF_QUIZ;
@@ -93,10 +107,10 @@ function plugin_install_quizzer()
 
 
 /**
-*   Loads the configuration records for the Online Config Manager
-*
-*   @return   boolean     true = proceed with install, false = an error occured
-*/
+ * Loads the configuration records for the Online Config Manager.
+ *
+ * @return  boolean     true = proceed with install, false = an error occured
+ */
 function plugin_load_configuration_quizzer()
 {
     global $_CONF, $_CONF_QUIZ, $_TABLES;
